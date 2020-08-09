f0c14aeb4f6ec091e0f2c9da753251b5891debc3
SLangrick <slangrick@hotmail.co.uk>
2020-08-09 17:12:56 +0100

    Added Download functionality and PHPMailer
    Allows download of cv and form automatically sends email using the PHPMailer class for an SMTP mail

---
+++ email.php
@@ -0,0 +1,29 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer.php';
+require 'SMTP.php';
+require 'POP3.php';
+require 'OAuth.php';
+require 'Exception.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'mail.slangrick.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'bot@slangrick.com';
+$mail->Password = 'botPassword2020';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('bot@slangrick.com', 'slangrick Website');
+$mail->addAddress('slangrick@hotmail.co.uk', 'Sam');
+
+$mail->isHTML(true);
+
+$mail->Subject = "Email from: " . $_POST['name'] . " " . $_POST['email'];
+$mail->Body = $_POST['subject'];
+$mail->send();
+header('Location: index.html');
+?>
